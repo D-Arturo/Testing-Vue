a72d16ea660c36816b549575f3f78c5f8c9ecb02
Arturo <arturomf88@gmail.com>
2021-02-23 14:56:42 +0100

    test verde

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+import testComponent from "@/components/testComponent.vue";
+
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("testComponent.vue", () => {
+  it("Title slot renders something", () => {
+    const title = "<span>What an awesome title</span>";
+    const wrapper = shallowMount(testComponent, {
+      slots: { Title:title }
+    });
+    expect(wrapper.html()).toMatch(title);
+  });
+});
